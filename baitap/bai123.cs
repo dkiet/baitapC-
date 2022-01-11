f18ebe0e5f0df6ca6862ff4e0cf4ca7538f8e769
Duong Van Kiet <dkiet742@gmail.com>
2022-01-11 09:37:16 +0700

    baitap

---
+++ baitap/bai123.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Bai123
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap vao so luong chu so: ");
+            int size = int.Parse(Console.ReadLine());
+            int[] number = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Nhap so[{0}]: ", i);
+                number[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("So nho nhat o vi tri: {0}", Find(number, size));
+        }
+
+        static int Find(int[] number, int size)
+        {
+            int min = number[0];
+            for (int i = 0; i < size; i++)
+            {
+                if (min > number[i])
+                {
+                    min = i - 1;
+                }
+            }
+            return min;
+        }
+    }
+}
