cf66f9f674cff8fc932c0403447bae354e614d33
Duong Van Kiet <dkiet742@gmail.com>
2022-01-12 21:09:10 +0700

    hoan thanh

---
+++ baitap/hambai115.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ham_Bai115
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap ho ten cua ban: ");
+            string name = Console.ReadLine();
+            Console.Write("Nhap diem toan cua ban: ");
+            float dToan = float.Parse(Console.ReadLine());
+            Console.Write("Nhap diem van cua ban: ");
+            float dVan = float.Parse(Console.ReadLine());
+            Console.WriteLine("Diem TB cua mon Toan va mon Van la: {0}", Average(dToan, dVan));
+        }
+
+        static float Average(float dToan, float dVan)
+        {
+            float average = (dToan + dVan) / 2;
+            return average;
+        }
+    }
+}
---
+++ lab/lab3cau5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab3_Cau5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap chieu dai quang duong: ");
+            int km = System.Int32.Parse(Console.ReadLine());
+
+            float tien1 = 1 * 15000;
+            float tien2 = (tien1) + ((km - 1) * 13500);
+            float tien3 = ((1 * 15000) + (4 * 13500)) + ((km - 5) * 11000);
+            float tien4 = tien3 * 0.9f;
+
+            if (km <= 1 && km > 0)
+            {
+                Console.WriteLine("Tong tien: {0} dong", tien1);
+            }
+            else if (km > 1 && km <= 5)
+            {
+                Console.WriteLine("Tong tien: {0} dong", tien2);
+            }
+            else if (km > 5 && km < 121)
+            {
+                Console.WriteLine("Tong tien: {0} dong", tien3);
+            }
+            else if (km > 120)
+            {
+                Console.WriteLine("Tong tien: {0} dong", tien4);
+            }
+            else
+            {
+                Console.WriteLine("Da di dau ma doi tinh tien");
+            }
+        }
+    }
+}
